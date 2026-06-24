c20edadac2a126b1c8d522bada86dcb54b61052f
akari <jvjdev@gmail.com>
2026-06-24 12:54:54 +0900

    Use headless JDK 25 for Clojure tools

--- overlays/clojure-jdk25.nix
+++ overlays/clojure-jdk25.nix
@@ -1,10 +1,10 @@
 final: prev: {
   clojure = prev.clojure.override {
-    jdk = final.jdk25;
+    jdk = final.jdk25_headless;
   };
 
   babashka = prev.babashka.override {
-    jdkBabashka = final.jdk25;
+    jdkBabashka = final.jdk25_headless;
 
     clojureToolsBabashka = prev.babashka.passthru.clojure-tools.override {
       inherit (final) clojure;
