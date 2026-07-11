702620f8d72600a9469ca0cd8d8bdbe4edc590f9
akari <jvjdev@gmail.com>
2026-07-11 16:37:30 +0900

    Add nix community cache

--- misc/setup/configuration.nix
+++ misc/setup/configuration.nix
@@ -44,7 +44,13 @@
     ];
 
     # Configure mirror.
-    substituters = [ "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" ];
+    substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
