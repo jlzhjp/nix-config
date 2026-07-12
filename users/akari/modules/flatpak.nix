f60b64023bb989ea532da46b375659c210c8e4f3
akari <jvjdev@gmail.com>
2026-07-12 19:20:49 +0900

    Add Anki

--- users/akari/modules/flatpak.nix
+++ users/akari/modules/flatpak.nix
@@ -14,22 +14,28 @@ _:
       "com.github.tchx84.Flatseal"
       "com.github.xournalpp.xournalpp"
       "com.obsproject.Studio"
+      "org.qbittorrent.qBittorrent"
+
+      "org.kde.kdenlive"
+      "info.smplayer.SMPlayer"
+
+      "net.ankiweb.Anki"
+      "md.obsidian.Obsidian"
+      "com.rustdesk.RustDesk"
+      "org.sqlitebrowser.sqlitebrowser"
+      "org.libreoffice.LibreOffice"
+
       "com.qq.QQ"
       "com.tencent.WeChat"
       "com.tencent.wemeet"
-      "org.qbittorrent.qBittorrent"
       "org.telegram.desktop"
+
       "com.valvesoftware.Steam"
       "net.lutris.Lutris"
       "com.github.Matoking.protontricks"
       "net.davidotek.pupgui2"
       "org.freedesktop.Platform.VulkanLayer.gamescope/x86_64/25.08"
-      "com.rustdesk.RustDesk"
-      "org.sqlitebrowser.sqlitebrowser"
-      "org.kde.kdenlive"
-      "info.smplayer.SMPlayer"
-      "org.libreoffice.LibreOffice"
-      "md.obsidian.Obsidian"
+
     ];
   };
 }
