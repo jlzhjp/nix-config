007fc686bbe75c3ee4bb41ec0c22aa88c3680aaa
akari <jvjdev@gmail.com>
2026-08-06 23:12:35 +0900

    Support kana input, add rime language model

--- users/akari/home.nix
+++ users/akari/home.nix
@@ -203,12 +203,52 @@
         jvjdev@gmail.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILK9mbY23GXiMBEuoOnRFHOVQbfjbkJDMYKMy+8Jgjc2
       '';
     };
-    dataFile = lib.mapAttrs' (
-      name: _:
-      lib.nameValuePair "fcitx5/rime/${name}" {
-        source = ./config/rime/${name};
-        force = true;
-      }
-    ) (lib.filterAttrs (_: type: type == "regular") (builtins.readDir ./config/rime));
+    dataFile =
+      lib.mapAttrs' (
+        name: _:
+        lib.nameValuePair "fcitx5/rime/${name}" {
+          # Rime uses source mtimes to decide whether compiled schemas are stale.
+          # Nix store files have epoch mtimes, so link to the live checkout here.
+          source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/users/akari/config/rime/${name}";
+          force = true;
+        }
+      ) (lib.filterAttrs (_: type: type == "regular") (builtins.readDir ./config/rime))
+      // {
+        "fcitx5/rime/essay-zh-hans.txt" = {
+          source = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/rime/rime-essay-simp/dc06d4c96ae72ad46b29e3aa824a5d1e8f721fd0/essay-zh-hans.txt";
+            hash = "sha256-k/wZF0P8K5tFSlzuDd4QKv7xpWb057MiifglgOTY86w=";
+          };
+          force = true;
+        };
+        "fcitx5/rime/zh-hans-t-essay-bgc.gram" = {
+          source = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/lotem/rime-octagram-data/f8ce3b534733e489a8470a7c2adf5a154e8ea069/zh-hans-t-essay-bgc.gram";
+            hash = "sha256-xS3rVrFkLk9WoYkm//y0twOFgo+SG/rJLF/0BVzOU3c=";
+          };
+          force = true;
+        };
+        "fcitx5/rime/zh-hans-t-essay-bgw.gram" = {
+          source = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/lotem/rime-octagram-data/f8ce3b534733e489a8470a7c2adf5a154e8ea069/zh-hans-t-essay-bgw.gram";
+            hash = "sha256-08skOMH9zWqFXdbKj1wQYKKSc8a2TCwsaa9nzXG2qn4=";
+          };
+          force = true;
+        };
+        "fcitx5/rime/zh-hant-t-essay-bgc.gram" = {
+          source = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/lotem/rime-octagram-data/97bf55046aad163c3d1881abae5312040b1bbed9/zh-hant-t-essay-bgc.gram";
+            hash = "sha256-fPZNUjepLcz5010Ex27zyRlFBgOmB6OI0VbucGPwRHA=";
+          };
+          force = true;
+        };
+        "fcitx5/rime/zh-hant-t-essay-bgw.gram" = {
+          source = pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/lotem/rime-octagram-data/97bf55046aad163c3d1881abae5312040b1bbed9/zh-hant-t-essay-bgw.gram";
+            hash = "sha256-BIjr1miPkAo5IA8reU8vmby/Ho/CcoCuSiMksIsVWcE=";
+          };
+          force = true;
+        };
+      };
   };
 }
